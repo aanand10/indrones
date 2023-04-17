bc11606a91bfd0b74372a0cf912407a7b9f91f07
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2023-04-18 04:08:23 +0530

    path

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -5,7 +5,7 @@ function Verify() {
   var user = document.getElementById("username").value;
   var pass = document.getElementById("password").value;
   if (user == userRef && pass == passRef) {
-    window.location.href = "./../../home.html";
+    window.location.href = "../../home.html";
   } else {
     alert("It's seem you make a mistake...Please see below the sign in btn");
   }
