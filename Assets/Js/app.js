f485b73aa399a1a95d42ae62d66f2df1d0577ea7
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2023-04-18 04:00:27 +0530

    js added

---
+++ Assets/Js/app.js
@@ -0,0 +1,12 @@
+function Verify() {
+  var userRef = "username";
+  var passRef = "indrones";
+
+  var user = document.getElementById("username").value;
+  var pass = document.getElementById("password").value;
+  if (user == userRef && pass == passRef) {
+    window.location.href = "./home.html";
+  } else {
+    alert("It's seem you make a mistake...Please see below the sign in btn");
+  }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## <b>Indrones</b> : Transforming your business with drone based solutions
+
+<hr/>
+
+- webpage link : [https://github.com/onlyanand10/indrones](https://github.com/onlyanand10/indrones)
+
+  - username : username
+  - password : indrones
+
+- My Personal website : [click me](https://onlyanand10.github.io/portfolio/)
